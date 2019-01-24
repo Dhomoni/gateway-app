968421461283ccb38784e99392779d4192ece2b5
mmohiahmed <mmohiahmed@gmail.com>
2019-01-24 15:17:12 +0600

    show first name of loggedin user

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -59,9 +59,9 @@ export class Home extends React.Component<IHomeProp, IHomeState> {
 
   render() {
     const { doctorList, account, match } = this.props;
-    console.log(JSON.stringify(doctorList));
     return (
       <div>
+        <this.renderLoggedInDataHomePageData account={account}/>
         <Form id="register-form" onSubmit={this.handleValidSubmit}>
           <FormGroup row>
             <Col sm="5">
@@ -156,110 +156,23 @@ export class Home extends React.Component<IHomeProp, IHomeState> {
         </div>
       </div>
     );
-
-{/*    return (
-      <Row>
-        <Col md="9">
-
-          <h2>
-            <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-          </h2>
-          <p className="lead">
-            <Translate contentKey="home.subtitle">This is your homepage</Translate>
-          </p>
-
-          { account && account.login ? this.renderLoggedInDataHomePageData(account) : this.renderLoggedOutHomePageData(account) }
-
-          <p>
-            <Translate contentKey="home.question">If you have any question on JHipster:</Translate>
-          </p>
-
-          <ul>
-            <li>
-              <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-                <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
-              </a>
-            </li>
-            <li>
-              <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-                <Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-                <Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>
-              </a>
-            </li>
-            <li>
-              <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-                <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/java_hipster" target="_blank" rel="noopener noreferrer">
-                <Translate contentKey="home.link.follow">follow @java_hipster on Twitter</Translate>
-              </a>
-            </li>
-          </ul>
-
-          <p>
-            <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
-            <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              Github
-            </a>
-            !
-          </p>
-
-        </Col>
-        <Col md="3" className="pad">
-          <span className="hipster rounded" />
-        </Col>
-      </Row>
-    );
-*/}
   }
 
-  renderLoggedOutHomePageData = ({ account }) =>
-    (
-      <div>
-        <Alert color="warning">
-          <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-          <Link to="/login" className="alert-link">
-            <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-          </Link>
-          <Translate contentKey="global.messages.info.authenticated.suffix">
-            , you can try the default accounts:
-            <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-            <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-          </Translate>
-        </Alert>
-
-        <Alert color="warning">
-          <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>
-          &nbsp;
-          <Link to="/register" className="alert-link">
-            <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-          </Link>
-        </Alert>
-      </div>
-    );
-
-  renderLoggedInDataHomePageData = ({ account }) =>
-    (
-      <div>
-        <Alert color="success">
-          <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
-            Welcome, {account.login}!
-          </Translate>
-        </Alert>
-      </div>
-    );
+  renderLoggedInDataHomePageData = ({ account }) => {
+    if (account && account.login) {
+      return (
+        <div>
+          <Alert color="success">
+            <Translate contentKey="home.logged.message" interpolate={{ name: account.firstName }}>
+              Welcome, {account.login}!
+            </Translate>
+          </Alert>
+        </div>
+      );
+    }
+    return null;
+  }
 }
-
-// const mapStateToProps = storeState => ({
-//   account: storeState.authentication.account,
-//   isAuthenticated: storeState.authentication.isAuthenticated
-// });
 
 const mapStateToProps = ({ authentication, doctor }: IRootState) => ({
   account: authentication.account,
